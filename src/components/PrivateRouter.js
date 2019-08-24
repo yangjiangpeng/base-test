50105461df8082df3eb855cbd5ae02fa587cd6e7
yangjiangpeng <176602174@qq.com>
2019-08-24 13:12:33 +0800

    login test

--- src/components/PrivateRouter.js
+++ src/components/PrivateRouter.js
@@ -4,7 +4,7 @@ import {useStoreState} from 'easy-peasy';
 
 export default({ component: Component, ...rest }) =>{
     const isAuted = useStoreState(state=>state.auth.isAuthed);
-    console.log(isAuted);
+    
     return (
       <Route
         {...rest}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,11 @@
 import React ,{useCallback} from 'react';
 import {useStoreActions} from 'easy-peasy';
 
-export default function Login(){
+export default function Login(props){
     const setLogin = useStoreActions(actions=>actions.auth.setLogin);
     const onSetLogin = ()=>{
         setLogin();
+        props.history.push('/');
     }
 
     return (
